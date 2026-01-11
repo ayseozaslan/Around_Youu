f9d760870a35fb7ebdee1c79c24d63d87a019aac
Ayşe Özaslan <113721001+ayseozaslan@users.noreply.github.com>
2026-01-11 03:05:38 +0300

    Delete AroundYou /app/src directory
    Temporarily hide source code

--- AroundYou /app/src/main/java/com/ayse/aroundyou/model/entities/MessageModel.kt
+++
@@ -1,7 +0,0 @@
-package com.ayse.aroundyou.model.entities
-
-data class MessageModel (
-    val message  :String,
-    var role : String
-){
-}
--- AroundYou /app/src/main/java/com/ayse/aroundyou/model/entities/MyUser.kt
+++
@@ -1,7 +0,0 @@
-package com.ayse.aroundyou.model.entities
-
-data class MyUser(
-    val uid: String = "",
-    val name: String = "",
-    val email: String = ""
-)
